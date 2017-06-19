abc69380734ec42953a2ea567235ac0bf56e6dcc
PHE <phejimlin@gmail.com>
2017-06-19 15:28:29 +0800

    Finish basic UI.

--- asus/demo/run.py
+++ asus/demo/run.py
@@ -3,5 +3,4 @@ import os
 
 app.secret_key = os.urandom(24)
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
-app.config['DEBUG'] = True
+app.run(host='0.0.0.0', port=port, debug=True)
