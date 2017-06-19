7a8856c2f7fd70b312b8b15813c1e50ea86045a9
PHE <phejimlin@gmail.com>
2017-06-19 17:51:16 +0800

    All label to svg.

--- asus/demo/run.py
+++ asus/demo/run.py
@@ -2,5 +2,5 @@ from blog import app
 import os
 
 app.secret_key = os.urandom(24)
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+port = int(os.environ.get('PORT', 80))
+app.run(host='0.0.0.0', port=port)
